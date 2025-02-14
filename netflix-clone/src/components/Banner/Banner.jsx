5ae48be634e0f3ef6214b582fd80bfb6ae341812
Biruktawit-Tasew <birukti.tasew@gmail.com>
2025-02-14 13:20:19 -0300

    axios and request

---
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import styles from "./banner.module.css";
+import axios from "../../utils/axios";
+import requests from "../../utils/requests";
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await axios.get(requests.fetchNetflixOriginals);
+        setMovie(
+          response.data.results[
+            Math.floor(Math.random() * response.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+  const truncate = (string, max_num) => {
+   return string?.length > max_num ? string.substr(0, max_num - 1) + "..." : string
+  };
+
+  return (
+    <div
+      className={styles.banner}
+      style={{
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+      }}
+    >
+      <div className={styles.banner_contents}>
+        <h1 className={styles.banner_title}>
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className={styles.banner_buttons}>
+          <button className={`${styles.banner_button} ${styles.play}`}>
+            Play
+          </button>
+          <button className={styles.banner_button}>My List</button>
+        </div>
+        <h1 className={styles.banner_description}>
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className={styles.banner_fadeBottom} />
+    </div>
+  );
+};
+
+export default Banner;
--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import NetflixLogo from '../../assets/images/netflix-3.svg'
 
 const Header = () => {
   return (
@@ -10,7 +11,9 @@ const Header = () => {
       <div className={styles.headerInnerContainer}>
         <div className={styles.headerLeftContainer}>
           <ul>
-            <li></li>
+            <li>
+              <img src={NetflixLogo} alt="NetflixLogo" width={100} />
+            </li>
             <li>Home</li>
             <li>TVShows</li>
             <li>Movies</li>
