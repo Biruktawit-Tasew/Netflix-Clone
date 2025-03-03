0e0b716eb5447f3955599ddddc16dcbfdaf13248
Biruktawit-Tasew <birukti.tasew@gmail.com>
2025-03-03 10:15:24 -0300

    redeploy

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -1,13 +1,26 @@
-import styles from './header.module.css'
+import styles from "./header.module.css";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import NetflixLogo from '../../assets/images/netflix-3.svg'
+import NetflixLogo from "../../assets/images/netflix-3.svg";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className={styles.headerOuterContainer}>
+    <div
+      className={`${styles.headerOuterContainer} ${
+        isScrolled ? styles.transparent : ""
+      }`}
+    >
       <div className={styles.headerInnerContainer}>
         <div className={styles.headerLeftContainer}>
           <ul>
@@ -41,6 +54,6 @@ const Header = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
